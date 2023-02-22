5a6ae739caff9ba9656a7976739c68bada866722
kosi <49681499+Tymekkos@users.noreply.github.com>
2023-02-22 21:18:03 +0100

    Add files via upload

---
+++ silnia/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int s=1;
+    cin >> n;
+    for(int i=1; i<=n; i++){
+        s=s*i;
+    }
+    cout << s;
+    return 0;
+}
