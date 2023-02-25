44ab7a2945255d8d8ff3a7974ec204bd3ccd9dfe
kosi <49681499+Tymekkos@users.noreply.github.com>
2023-02-25 15:18:06 +0100

    Add files via upload

---
+++ buzka/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "    ////    " << endl;
+    cout << " ( O   O ) " << endl;
+    cout << "----(_)----" << endl;
+    return 0;
+}
