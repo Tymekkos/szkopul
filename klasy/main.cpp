176ff3e583f5b460a846ac6833f733a9e9ced7dc
kosi <49681499+Tymekkos@users.noreply.github.com>
2023-02-28 14:39:46 +0100

    Add files via upload

---
+++ klasy/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cin >> a >> b >> c;
+    cout << a << " " << b << " " << c << endl;
+    cout << "a " << a << endl;
+    cout << "b " << b << endl;
+    cout << "c " << c << endl;
+    return 0;
+}
